09478712118de1b8896f80618d6376d05323d1d8
Sandra <mojsova.sandra@yahoo.com>
2021-10-25 17:33:11 +0200

    Second homework-changes

--- homework02/c03.js
+++ homework02/c03.js
@@ -4,49 +4,66 @@ const api = express();
 
 api.use(express.json());
 
-let students = [
-    { id: 0, first_name: "Sandra", gpa: 10 }
-];
+let students = [];
 
 api.get('/students', (req, res) => {
     res.status(200).send(students);
 })
 
 api.get('/students/:id', (req, res) => {
-    if (!students[req.params.id]) {
-        return res.status(404).send(students)
+    let output = students.filter(student => student.id === Number(req.params.id));
+    if (!output[0]) {
+        return res.status(404).send('Not found');
     }
-    return res.status(200).send([students[req.params.id]])
+    return res.status(200).send(output[0]);
 })
 
 api.post('/students', (req, res) => {
-    students = [...students, {
-        ...req.body, id: students.length,
-    }]
-    res.status(200).send({ ...req.body, id: students.length });
+    let id = 1;
+    if (students.length !== 0) {
+        id = students[students.length - 1].id + 1;
+    }
+    let student = {
+        id,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        gpa: req.body.gpa
+    };
+    students = [...students, student];
+    res.status(200).send(student);
 })
 
 api.delete('/students/:id', (req, res) => {
-    if (!students[req.params.id]) {
+    let prevLength = students.length;
+    students = students.filter(student => student.id !== Number(req.params.id));
+    if (students.length === prevLength) {
         return res.status(404).send('Not Found');
     }
-    students = students.filter((student, index) => index != req.params.id);
     res.status(204).send();
 });
 
 api.put("/students/:id", (req, res) => {
-    let index = students.findIndex((student, id) => id == req.params.id);
-    let updatedStudent = { ...req.body, id: req.params.id }
-    //console.log(updatedStudent);
-    students[index] = updatedStudent;
-    res.status(200).send(updatedStudent);
+    students = students.map(s => {
+        if (s.id === Number(req.params.id)) {
+            s.first_name = req.body.first_name;
+            s.last_name = req.body.last_name;
+            s.gpa = req.body.gpa;
+        }
+        return s;
+    })
+    res.status(204).send();
 });
 
 api.patch('/students/:id', (req, res) => {
-    let index = students.findIndex((student, id) => id == req.params.id);
-    let updatedStudent = { ...students[index], gpa: req.body.gpa };
-    students[index] = updatedStudent;
-    res.status(200).send(updatedStudent)
+    students = students.map(s => {
+        if (s.id === Number(req.params.id)) {
+            s.first_name = req.body.first_name ? req.body.first_name : s.first_name;
+            s.last_name = req.body.last_name ? req.body.last_name : s.last_name;
+            s.gpa = req.body.gpa ? req.body.gpa : s.gpa;
+        }
+        return s;
+    });
+    res.status(204).send();
 })
 
 api.listen(10000, (err) => {
--- homework02/index.js
+++ homework02/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const api = express();
 api.use(express.json());
 
-const Read = (fileName) => {
+const read = (fileName) => {
     return new Promise((success, fail) => {
         fs.readFile(fileName, 'utf8', (err, data) => {
             if (err) {
@@ -15,9 +15,9 @@ const Read = (fileName) => {
         });
 
     })
-}
+};
 
-const Write = (fileName, writing) => {
+const write = (fileName, writing) => {
     return new Promise((success, fail) => {
         fs.writeFile(fileName, writing, 'utf-8', (err) => {
             if (err) {
@@ -28,109 +28,123 @@ const Write = (fileName, writing) => {
         });
 
     })
-}
+};
 
 
 api.get('/students', async (req, res) => {
     try {
-        let data = await Read('students.json')
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
         res.status(200).send(parsedData);
     }
     catch (err) {
         console.log(err);
-        res.status(400).send(err);
+        res.status(500).send(err);
     }
-})
+});
 
 api.post('/students', async (req, res) => {
     try {
-        let data = await Read('students.json')
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
-        let output = [...parsedData, {
-            ...req.body, id: parsedData.length,
-        }];
-        let outputString = JSON.stringify(output);
-        await Write('students.json', outputString);
-        res.status(201).send(req.body)
+        let id = 1;
+        if (parsedData.length !== 0) {
+            id = parsedData[parsedData.length - 1].id + 1;
+        }
+        let student = {
+            id,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            gpa: req.body.gpa
+        };
+        parsedData = [...parsedData, student];
+        let outputString = JSON.stringify(parsedData);
+        await write('students.json', outputString);
+        res.status(201).send(student);
+    } catch (err) {
+        res.status(500).send(err);
     }
-    catch (err) {
-        res.send(err);
-    }
-})
+});
 
 api.get('/students/:id', async (req, res) => {
     try {
-        let data = await Read('students.json')
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
-        if (!parsedData[req.params.id]) {
+        let output = parsedData.filter(student => student.id === Number(req.params.id));
+        if (!output[0]) {
             return res.status(404).send('Not Found');
         }
-        res.status(200).send(parsedData[req.params.id])
+        let outputString = JSON.stringify(output[0]);
+        res.status(200).send(outputString);
     }
     catch (err) {
-        res.send(err);
+        res.status(500).send(err);
     }
-})
+});
 
 api.delete('/students/:id', async (req, res) => {
     try {
-        let data = await Read('students.json');
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
-        if (!parsedData[req.params.id]) {
-            return res.status(404).send('Not Found');
+        let prevLength = parsedData.length;
+        parsedData = parsedData.filter(student => student.id !== Number(req.params.id));
+        if (parsedData.length === prevLength) {
+            return res.status(404).send('not found');
         }
-        let output = parsedData.filter((s, id) => id != req.params.id);
-        let outputString = JSON.stringify(output);
-        await Write('students.json', outputString);
+        let outputString = JSON.stringify(parsedData);
+        await write('students.json', outputString);
         res.status(204).send();
     }
     catch (err) {
-        res.send(err);
+        res.status(500).send(err);
     }
-})
+});
 
 api.put('/students/:id', async (req, res) => {
     try {
-        let id = req.params.id;
-        let data = await Read('students.json');
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
-        let index = parsedData.findIndex((student, index) => index == id);
-        //console.log(index);
-        let updatedStudent = { ...req.body, id: Number(id) };
-        // console.log(updatedStudent);
-        parsedData[index] = updatedStudent;
+        parsedData = parsedData.map(student => {
+            if (student.id === Number(req.params.id)) {
+                student.first_name = req.body.first_name;
+                student.last_name = req.body.last_name;
+                student.gpa = req.body.gpa;
+            }
+            return student;
+        });
         let outputString = JSON.stringify(parsedData);
-        await Write('students.json', outputString);
-        res.status(200).send(updatedStudent);
+        await write('students.json', outputString);
+        res.status(204).send();
     }
     catch (err) {
-        res.send(err);
+        res.status(500).send(err);
     }
-})
+});
 
 api.patch('/students/:id', async (req, res) => {
     try {
-        let id = req.params.id;
-        let data = await Read('students.json');
+        let data = await read('students.json');
         let parsedData = JSON.parse(data);
-        let index = parsedData.findIndex((student, index) => index == id);
-        //console.log(index);
-        let updatedStudent = { ...parsedData[index], gpa: req.body.gpa };
-        // console.log(updatedStudent);
-        res.send(updatedStudent);
-        parsedData[index] = updatedStudent;
+        parsedData = parsedData.map(student => {
+            if (student.id === Number(req.params.id)) {
+                student.first_name = req.body.first_name ? req.body.first_name : student.first_name;
+                student.last_name = req.body.last_name ? req.body.last_name : student.last_name;
+                student.gpa = req.body.gpa ? req.body.gpa : student.gpa;
+            }
+            return student;
+        });
         let outputString = JSON.stringify(parsedData);
-        await Write('students.json', outputString);
+        await write('students.json', outputString);
+        res.status(204).send();
     }
     catch (err) {
-        res.send(err);
+        res.status(500).send(err);
     }
-})
+});
 
 api.listen(8000, err => {
     if (err) {
         console.log(err);
     }
     return console.log('server successfully started on port 8000');
-})
+});
